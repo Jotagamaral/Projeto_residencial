6bc1ff71ace5527b3d3c7a1a0d2129ed8d122173
João Gabriel <jota.amaralmiranda@gmail.com>
2026-03-30 14:52:44 -0300

    Fix: Delete old & unused files

--- backend/src/main/java/condosync/backend/repository/LocaisRepository.java
+++
@@ -1,8 +0,0 @@
-package condosync.backend.repository;
-
-import condosync.backend.model.Locais;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocaisRepository extends JpaRepository<Locais, Long> {
-    Locais findByNome(String nome);
-}
