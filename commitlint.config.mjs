6f51e05c0aa9d0d90f164e5bb8b981e9749ddc4c
João Gabriel <jota.amaralmiranda@gmail.com>
2026-04-28 16:26:53 -0300

    fix: modifica commit

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -4,6 +4,7 @@ export default {
         (message) => message.includes('Merge'),
         (message) => message.includes('chore(release)'),
         (message) => message.includes('chore(deps)'),
+        (message) => message.includes('chore(deps-dev)'),
         (message) => message.includes('[skip ci]'),
         (message) => message.includes('ci:'),
     ],
